f031f767508edd5750642deb3db41b6db16dca47
ShlomoNus <shlomonuswork@gmail.com>
2024-12-05 23:50:36 +0200

    base

---
+++ eslint.config.js
@@ -0,0 +1,140 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      "no-control-regex": "off",
+      "prefer-const": "error",
+      "react/jsx-curly-brace-presence": [
+        "warn",
+        { props: "never", children: "never" },
+      ],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/function-component-definition": [
+        2,
+        {
+          namedComponents: "function-declaration",
+          unnamedComponents: "function-expression",
+        },
+      ],
+      "react/self-closing-comp": ["error", { component: true, html: true }],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "max-params": ["error", 2],
+      "max-lines": ["warn", { max: 100 }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "no-debugger": "warn",
+      "arrow-body-style": ["error", "as-needed"],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "class",
+          format: ["PascalCase"],
+        },
+        {
+          selector: "typeAlias",
+          format: ["PascalCase"],
+        },
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+        },
+        {
+          selector: "variable",
+          format: ["camelCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        {
+          selector: "function",
+          format: ["camelCase", "PascalCase"],
+        },
+        { selector: "enum", format: ["PascalCase"] },
+        {
+          selector: "typeLike",
+          format: ["PascalCase"], // Enforces PascalCase for types and components
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "capitalized-comments": [
+        "warn",
+        "always",
+        {
+          ignoreConsecutiveComments: true,
+        },
+      ],
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "return" },
+        { blankLine: "always", prev: "*", next: "block" },
+        { blankLine: "always", prev: "for", next: "*" },
+        { blankLine: "always", prev: "*", next: "for" },
+        { blankLine: "always", prev: "if", next: "*" },
+        { blankLine: "always", prev: "*", next: "if" },
+        {
+          blankLine: "always",
+          prev: ["const", "let", "var"],
+          next: "*",
+        },
+        {
+          blankLine: "any",
+          prev: ["const", "let", "var"],
+          next: ["const", "let", "var"],
+        },
+        { blankLine: "always", prev: "import", next: "*" },
+        { blankLine: "never", prev: "import", next: "import" },
+        { blankLine: "always", prev: "export", next: "*" },
+        { blankLine: "never", prev: "export", next: "export" },
+        { blankLine: "always", prev: "function", next: "*" },
+        { blankLine: "always", prev: "*", next: "function" },
+        { blankLine: "always", prev: "*", next: "export" },
+      ],
+      "lines-between-class-members": ["error", "always"],
+      "lines-around-comment": [
+        "error",
+        {
+          beforeBlockComment: true,
+          beforeLineComment: true,
+          allowBlockStart: true,
+          allowBlockEnd: true,
+          allowObjectStart: true,
+          allowObjectEnd: true,
+          allowArrayStart: true,
+          allowArrayEnd: true,
+        },
+      ],
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-misused-promises": "error",
+      "@typescript-eslint/promise-function-async": "error",
+    },
+  }
+);
