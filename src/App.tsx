49be4dd3858feffcb8b08c8c5d27af1d624d42bb
ShlomoNus <shlomonuswork@gmail.com>
2025-01-23 09:53:01 +0200

    delete unneeded code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
-import Button from "@mui/material/Button";
 
 function App() {
-  const X = 2;
 
-  console.log(X);
 
   return (
     <>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+
     </>
   );
 }
